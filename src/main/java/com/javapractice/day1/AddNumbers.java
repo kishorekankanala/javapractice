2ac0de1cdc6ca1abd3e34e48d2db6b6618b6ac57
kanka@kishore <kankanalakishore9@gmail.com>
2023-04-24 13:20:29 -0400

    added extra log message and multiplication of table added

---
+++ src/main/java/com/javapractice/day1/AddNumbers.java
@@ -0,0 +1,9 @@
+package com.javapractice.day1;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("add num is "+(num1+num2));
+    }
+}
