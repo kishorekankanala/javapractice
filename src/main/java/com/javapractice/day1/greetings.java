1731672b8ab379bd900ba957d20eb3b958cf0129
kanka@kishore <kankanalakishore9@gmail.com>
2023-04-24 13:19:58 -0400

    added extra log message and multiplication of table added

--- src/main/java/com/javapractice/day1/greetings.java
+++ src/main/java/com/javapractice/day1/greetings.java
@@ -2,6 +2,10 @@ package com.javapractice.day1;
 
 public class greetings {
     public static void main(String[] args) {
-        System.out.println("wellcom to jswoeld");
+        String username = "User";
+        if (args.length>0){
+            username = args[0];
+        }
+        System.out.println("hi"+username+",\n\twellcom to jsword");
     }
 }
