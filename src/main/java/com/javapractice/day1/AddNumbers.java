64743129689f0290a2e3c43851e79a510790cf12
kishorekankanala <130674398+kishorekankanala@users.noreply.github.com>
2023-04-24 14:55:29 -0400

    Update AddNumbers.java

--- src/main/java/com/javapractice/day1/AddNumbers.java
+++ src/main/java/com/javapractice/day1/AddNumbers.java
@@ -4,6 +4,7 @@ public class AddNumbers {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 20;
+        int num3 = 330;
         System.out.println("add num is "+(num1+num2));
     }
 }
